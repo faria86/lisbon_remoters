2e2c5565a78ae423f6c5e55d6e06f5dc1d9ce05b
luisfaria <lfariaxxi@hotmail.com>
2020-05-21 19:32:27 +0100

    lots of css color border home forms

--- routes/comment.js
+++ routes/comment.js
@@ -6,13 +6,12 @@ const commentRouter = new express.Router();
 
 const routeGuard = require("./../middleware/route-guard");
 
-//const Place = require('./../models/place');
 const Comment = require("./../models/comment");
 
 commentRouter.post("/:placeId", routeGuard, (req, res, next) => {
   const placeId = req.params.placeId;
   const content = req.body.content;
-
+ 
   Comment.create({
     content,
     creator: req.user._id,
